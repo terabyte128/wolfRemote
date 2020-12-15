62992993e45996b05fc407a8e4e3bbe453f18a62
Sam Wolfson <wolfson@cs.uw.edu>
2020-12-14 22:25:10 -0800

    Update name in API docs

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
     API_URL,
     config={  # Swagger UI config overrides
-        'app_name': "WolfRemote"
+        'app_name': "wolfRemote"
     },
 )
 
