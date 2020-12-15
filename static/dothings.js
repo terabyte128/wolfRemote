5fa4aecb74057e1d61cc9e8b710bc63dabf82ac5
Sam Wolfson <wolfson@cs.uw.edu>
2020-12-14 22:25:42 -0800

    Improve UI and update automatically.
    - Set light sliders by default to average of their group
    - Set HSB sliders to average of all lights
    - Automatically update sliders on focus

--- static/dothings.js
+++ static/dothings.js
@@ -1,8 +1,34 @@
-function updateSlider() {
+function updateBacklightSlider() {
     fetch("/api/v1/tv/backlight").then((response) => {
         if (response.ok) {
             response.json().then(json => {
                 document.querySelector('#backlight-slider').value = json['current'];
+            })
+        }
+    });
+}
+
+function avg(...xs) {
+    return xs.reduce((a, b) => a + b) / xs.length;
+}
+
+function updateLightBrightnessSlider() {
+    fetch("/api/v1/lights").then(response => {
+        if (response.ok) {
+            response.json().then(json => {
+                let lrAvg = avg(json['lr1']['brightness'], json['lr2']['brightness']);
+                let drAvg = avg(json['dr1']['brightness'], json['dr2']['brightness']);
+
+                document.querySelector("#living-room").value = lrAvg;
+                document.querySelector("#dining-room").value = drAvg;
+
+                let hue = avg(...Object.keys(json).map(k => json[k]['hue']));
+                let saturation = avg(...Object.keys(json).map(k => json[k]['saturation']));
+                let kelvin = avg(...Object.keys(json).map(k => json[k]['kelvin']));
+
+                document.querySelector("#hue").value = hue;
+                document.querySelector("#saturation").value = saturation;
+                document.querySelector("#kelvin").value = kelvin;
             })
         }
     })
@@ -91,36 +117,44 @@ function setLightBrightness() {
     makeRequest("/api/v1/lights", 'put', request);
 }
 
+function setBacklight(e) {
+    let data = e.target.dataset;
+    makeRequest(data.url, data.method, {
+        "backlight": parseInt(e.target.value)
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    // continuously update the backlight slider
-    updateSlider();
+    // update sliders
+    updateBacklightSlider();
+    updateLightBrightnessSlider();
+
+    // make a generic request when certain buttons are pressed
     document.querySelectorAll("button.make-request").forEach(btn => {
         btn.addEventListener('click', () => makeRequest(btn.dataset.url, btn.dataset.method, btn.dataset));
     })
 
-    function setBacklight(e) {
-        let data = e.target.dataset;
-        makeRequest(data.url, data.method, {
-            "backlight": parseInt(e.target.value)
-        });
-    }
+    // listen for changes on backlight slider
+    document.querySelector('#backlight-slider').addEventListener('mouseup', setBacklight);
+    document.querySelector('#backlight-slider').addEventListener('touchend', setBacklight);
 
-    document.querySelector('#backlight-slider').onmouseup = setBacklight;
-    document.querySelector('#backlight-slider').ontouchend = setBacklight;
-
+    // listen for changes on light sliders
     document.querySelectorAll("input.light-slider").forEach(ls => {
         ls.addEventListener('mouseup', setLightBrightness);
         ls.addEventListener('touchend', setLightBrightness);
     });
 
+    // listen for clicks to lights that control buttons
     document.querySelectorAll("button.btn-light").forEach(lb => {
         lb.addEventListener('click', () => updateHSK(lb.dataset));
     });
 
+    // try to stop buttons from being highlighted when clicking
     document.querySelectorAll("button").forEach(b => {
         b.addEventListener('click', b.blur);
     });
 
+    // super special developer settings
     document.querySelector("#toggle-debug").addEventListener('click', () => {
         var x = document.querySelector("#debug-options");
         if (x.style.display === "none") {
@@ -128,6 +162,32 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
           x.style.display = "none";
         }
+    });
+
+    // keep track of focus so that focus events are debounced
+    window.addEventListener('blur', () => focused = false);
+
+    // onfocus, update sliders
+    window.addEventListener('focus', function() {
+        if (!focused) {
+            // update sliders on focus
+            updateBacklightSlider();
+            updateLightBrightnessSlider();
+            focused = true;
+        }
+    })
+
+    // set the current tab based on the tab before reload
+    if (currentTab = localStorage.getItem('currentTab')) {
+        var tab = new bootstrap.Tab(document.querySelector(`#${currentTab}`));
+        tab.show();
+    }
+
+    // track current tab when switched
+    document.querySelectorAll('a[data-bs-toggle="tab"]').forEach(tab => {
+        tab.addEventListener('shown.bs.tab', function (event) {
+            localStorage.setItem('currentTab', event.target.id);
+        });
     });
 });
 
